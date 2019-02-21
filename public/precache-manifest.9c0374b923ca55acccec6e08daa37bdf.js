253a2759955d27a97ee56284ba4a42d1d63c069a
Ian Berns <iberns@tamu.edu>
2019-02-21 20:41:47 +0000

    removed hi everyone from app.js

---
+++ public/precache-manifest.9c0374b923ca55acccec6e08daa37bdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f30ad661e62991882324",
+    "url": "/static/css/main.8c51b9db.chunk.css"
+  },
+  {
+    "revision": "f30ad661e62991882324",
+    "url": "/static/js/main.f30ad661.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "00c94a935bc2a54d26e591345c23a3ba",
+    "url": "/index.html"
+  }
+];

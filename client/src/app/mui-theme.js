ccfb12c308360bb451396eb665877b598a9108bb
Stephanie Keck <stephaniehkeck@gmail.com>
2019-02-24 16:32:35 -0600

    add colors to mui theme

--- client/src/app/mui-theme.js
+++ client/src/app/mui-theme.js
@@ -1,7 +1,13 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import cyan from '@material-ui/core/colors/cyan';
 
 const theme = createMuiTheme({
-	
+	palette: {
+		primary: {
+			main: '#9e9e9e'
+		},
+		secondary: cyan
+	}
 });
 
 export default theme;

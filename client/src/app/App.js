bdff8798c5bd570a2c4622abf614d86447f0dec3
Stephanie Keck <stephaniehkeck@gmail.com>
2019-02-24 16:38:48 -0600

    add css baseline

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+
 //import { Provider } from "react-redux";
 //import store from "../state/store";
+
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import theme from './mui-theme';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import { AppLayout } from '../views/layouts/app-layout';
 import { BrowserRouter as Router}  from 'react-router-dom';
 import Routes from './routes';
@@ -11,6 +15,7 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
+        <CssBaseline />
         <AppLayout>
           <Router>
             <Routes />

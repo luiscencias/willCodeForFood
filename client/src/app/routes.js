e16d2035ae5137b6207690d935ddc88259036d2d
kylansnyder <37723261+kylansnyder@users.noreply.github.com>
2019-03-16 12:30:14 -0500

    Fixed deleted routes

---
+++ client/src/app/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+    Route,
+    Switch
+} from 'react-router-dom';
+
+import { Home } from '../views/home';
+import { NotFound } from '../views/notfound';
+import { About } from '../views/about';
+import { Gallery } from '../views/gallery';
+
+export default () => (
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route path="/gallery" component={Gallery} />
+
+
+
+
+
+        /* 	keep not found route below everything else	*/
+        <Route path="*" component={NotFound} />
+    </Switch>
+);

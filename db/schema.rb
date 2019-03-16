d09bb68b0c79a043917f63ab5c9d8ff083b4a046
kylansnyder <37723261+kylansnyder@users.noreply.github.com>
2019-03-16 12:44:35 -0500

    Created member table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_03_16_174008) do
+
+  create_table "members", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "phone_number"
+    t.string "graduation"
+    t.integer "year"
+    t.string "major"
+    t.boolean "member", default: false
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

1bec01154e64d1aa2a2317a454cf94edfbb4afa2
iberns@tamu.edu <iberns@tamu.edu>
2019-03-18 15:44:58 -0500

    Blake and Ian shop look

--- client/src/views/shop/shop.js
+++ client/src/views/shop/shop.js
@@ -1,28 +1,59 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './shop.css';
 
 import {useScrollToTopOnMount} from "../../helpers";
 
 class Shop extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fname: '',
+            lname: '',
+            email: '',
+            subject: ''
+
+        }
+    }
+
+    handleChange (event) {
+        event.preventDefault();
+    }
+
     render() {
+
         return (
-            <div className="App">
+
+            <div className="Shop">
                 <p>PBE Official Store</p>
                 <div>
                     <form action="/action_page.php">
                         <label>First Name</label>
-                        <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+                        <input type="text" id="fname" name="fname" placeholder="Your name.." required={true}
+                                value = {this.state.fname}
+                                onChange={e => this.setState({fname: e.target.value})}
+
+                        />
                         <label>Last Name</label>
-                        <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+                        <input type="text" id="lname" name="lname" placeholder="Your last name.." required={true}
+                               value = {this.state.lname}
+                               onChange={e => this.setState({lname: e.target.value})}
+                        />
 
 
                         <label>Email</label>
-                        <input type="email" id="email" name="email" placeholder="Your email" />
+                        <input type="email" id="email" name="email" placeholder="Your email" required={true}
+                               value = {this.state.email}
+                               onChange={e => this.setState({email: e.target.value})}
+                        />
 
 
-                        <label>Subject</label>
-                        <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-                        <input type="submit" value="Submit" />
+                        <label>Clothing Order</label>
+                        <textarea id="subject" name="subject" placeholder="Item No.: Size: Qty:" required={true}
+                                  value = {this.state.subject}
+                                  onChange={e => this.setState({subject: e.target.value})}
+                        ></textarea>
+                        <input type="submit" value="Submit" required={true} />
                     </form>
                 </div>
             </div>

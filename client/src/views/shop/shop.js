7bd5ea3bda47e5fb1af738a82f7c2d7c53f75ea8
Blake Stolle <blake.stolle.1@tamu.edu>
2019-03-18 18:21:00 -0500

    post spring-break group merge

--- client/src/views/shop/shop.js
+++ client/src/views/shop/shop.js
@@ -7,25 +7,13 @@ import Section from './Section';
 import {useScrollToTopOnMount} from "../../helpers";
 
 class Shop extends React.Component {
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
     constructor(props) {
         super(props);
         this.state = {
             fname: '',
             lname: '',
             email: '',
-<<<<<<< Updated upstream
-            subject: ''
-
-        }
-    }
-
-    handleChange (event) {
-        event.preventDefault();
-=======
+            subject: '',
             message: '',
             mailSent: false,
             error: null
@@ -35,49 +23,32 @@ class Shop extends React.Component {
     handleFormSubmit( event ) {
         event.preventDefault();
         console.log(this.state);
->>>>>>> Stashed changes
     }
 
     render() {
 
         return (
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
             <div className="Shop">
                 <p>PBE Official Store</p>
                 <div>
                     <form action="#">
                         <label>First Name</label>
-<<<<<<< Updated upstream
                         <input type="text" id="fname" name="fname" placeholder="Your name.." required={true}
                                 value = {this.state.fname}
                                 onChange={e => this.setState({fname: e.target.value})}
 
                         />
+
+
                         <label>Last Name</label>
                         <input type="text" id="lname" name="lname" placeholder="Your last name.." required={true}
                                value = {this.state.lname}
                                onChange={e => this.setState({lname: e.target.value})}
-=======
-                        <input type="text" id="fname" name="firstname" placeholder="Your name.." required={true}
-                               value={this.state.fname}
-                               onChange={e => this.setState({ fname: e.target.value })}
-                        />
-
-
-                        <label>Last Name</label>
-                        <input type="text" id="lname" name="lastname" placeholder="Your last name..." required={true}
-                               value={this.state.lname}
-                               onChange={e => this.setState({ lname: e.target.value })}
->>>>>>> Stashed changes
                         />
 
 
                         <label>Email</label>
                         <input type="email" id="email" name="email" placeholder="Your email" required={true}
-<<<<<<< Updated upstream
                                value = {this.state.email}
                                onChange={e => this.setState({email: e.target.value})}
                         />
@@ -88,21 +59,9 @@ class Shop extends React.Component {
                                   value = {this.state.subject}
                                   onChange={e => this.setState({subject: e.target.value})}
                         ></textarea>
-                        <input type="submit" value="Submit" required={true} />
-=======
-                               value={this.state.email}
-                               onChange={e => this.setState({ email: e.target.value })}
-                        />
 
-
-
-                        <label>Message</label>
-                        <textarea id="message" name="message" placeholder="Write something.." required={true}
-                                  value={this.state.message}
-                                  onChange={e => this.setState({ message: e.target.value })}
-                        ></textarea>
                         <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Submit" />
->>>>>>> Stashed changes
+
                     </form>
                 </div>
             </div>

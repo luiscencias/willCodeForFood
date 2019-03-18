61c966a54148228858a50340a7139bab7e4bb8e5
kylansnyder <37723261+kylansnyder@users.noreply.github.com>
2019-03-17 22:40:30 -0500

    Apply form finished

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,24 @@
 class MembersController < ApplicationController
+
+  def member_params
+    params.require(:member).permit(:first_name, :last_name, :email, :phone_number, :graduation_year, :major, :member_status, :password)
+  end
+
+  def create
+    @member = Member.new(member_params)
+
+    if @member.save
+      render json: @member, status: :created
+    else
+      render json: @member.errors, status: :unprocessable_entity
+    end
+
+  end
+
+  def index
+    @member = Member.all
+
+    render json: @member
+  end
+
 end
--- client/src/views/application/ApplyForm.js
+++ client/src/views/application/ApplyForm.js
@@ -17,18 +17,41 @@ import GraduationField from "./GraduationField";
 import MajorField from "./MajorField";
 
 const validationSchema = Yup.object({
+
+	firstName: Yup
+		.string("Enter your name")
+		.required("Name is required"),
+	lastName: Yup
+		.string("Enter your name")
+		.required("Name is required"),
+	phoneNumber: Yup
+		.string("Enter your phone number")
+		.required("Phone number is required"),
+	graduationYear: Yup
+		.string("Enter your graduation year")
+		.required("Graduation year is required"),
+	major: Yup
+		.string("Enter your major")
+		.required("Major is required"),
 	email: Yup
 		.string("Enter your email")
 		.email("You must enter a valid email")
 		.required("Email is required"),
 	password: Yup
 		.string("Enter your password")
-		.required("Password is required")
+		.required("Password is required"),
 });
 
 const initialValues = {
+	firstName: '',
+	lastName: '',
 	email: '',
-	password: ''
+	phoneNumber: '',
+	graduationYear: '',
+	major: '',
+	password: '',
+
+
 };
 
 const ApplyForm = ({ handleSubmit }) => {
@@ -51,7 +74,6 @@ const ApplyForm = ({ handleSubmit }) => {
 					<PasswordField />
 					<PasswordField label = "Re-enter Password"/>
 					<SubmitButton />
-
 				</Form>
 			)}
 		</Formik>

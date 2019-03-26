657961e3cad63a480f22bc8ef2a32bd63cb34a28
kylansnyder <37723261+kylansnyder@users.noreply.github.com>
2019-03-26 13:37:54 -0500

    Database updated

--- db/migrate/20190318204438_create_members.rb
+++ db/migrate/20190318204438_create_members.rb
@@ -1,15 +1,24 @@
 class CreateMembers < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :members
-
+  def up
     create_table :members do |t|
-      t.string :email
-      t.string :password_digest
+
       t.string :first_name
       t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.integer :phone_number
+      t.integer :graduation_year
+      t.string :major
+      t.boolean :is_member
 
       t.timestamps
     end
     add_index :members, :email, unique: true
+
+    change_column :members, :is_member, :boolean, :default => false
+  end
+
+  def down
+    drop_table :members
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_205207) do
+ActiveRecord::Schema.define(version: 2019_03_18_204438) do
 
-  create_table "fnames", force: :cascade do |t|
-    t.string "lname"
+  create_table "members", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
-    t.string "subject"
+    t.string "password_digest"
+    t.integer "phone_number"
+    t.integer "graduation_year"
+    t.string "major"
+    t.boolean "is_member", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_members_on_email", unique: true
   end
 
 end

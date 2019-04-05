2ae5b29214fc7fb7f77208e70159ed8d28c0fe05
jonathan.mejia <jonathan.mejia@tamu.edu>
2019-04-05 13:43:44 -0500

    fixed json rendering

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,23 +9,23 @@ class EventsController < ApplicationController
       page = params[:page] || 1
       @events = Event.all.paginate(page: params[:page], per_page: 10)
     end
-    render json: @events  # returns all events; default action for component to retrieve data; consider ordering by date
+    render json: @events.to_json  # returns all events; default action for component to retrieve data; consider ordering by date
   end
 
   # GET /events/1
   def show
     id = params[:id]
     @event = Event.find(id)
-    render json: @event # show specific event/ (perhaps a stretch)
+    render json: @event.to_json # show specific event/ (perhaps a stretch)
   end
   
   def create # create new event
     @event = Event.new(event_params)
 
     if @event.save
-      render json: @event, status: :created, location: @event
+      render json: @event.to_json, status: :created, location: @event
     else
-      render json: @event.errors, status: :unprocessable_entity
+      render json: @event.errors.to_json, status: :unprocessable_entity
     end
   end
 
@@ -33,9 +33,9 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
     if @event.update(event_params)
-      render json: @event
+      render json: @event.to_json
     else
-      render json: @event.errors, status: :unprocessable_entity
+      render json: @event.errors.to_json, status: :unprocessable_entity
     end
   end
 

167d5eeecf7db7e78fff3b967276bc4a609ecec3
kylansnyder <37723261+kylansnyder@users.noreply.github.com>
2019-04-07 15:41:49 -0500

    Fixed Gemfile

--- app/admin/members.rb
+++ app/admin/members.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register Member do
 
   permit_params :email, :password_digest, :first_name, :last_name, :phone_number, :graduation_year, :major, :is_member, :points
 
+  menu label: "My Posts"
   #action_item :approve_member, only: :show do
    # link_to "Approve",
   #end

91537c94f2ccad416aff3cd457366806b8d93c47
kylansnyder <37723261+kylansnyder@users.noreply.github.com>
2019-04-09 13:14:24 -0500

    Added admin approve/reject features

--- app/admin/members.rb
+++ app/admin/members.rb
@@ -3,23 +3,44 @@ ActiveAdmin.register Member do
   permit_params :email, :password_digest, :first_name, :last_name, :phone_number, :graduation_year, :major, :member_status, :points
 
   show title: :first_name
-  #index do
-  #  selectable_column
 
-    #columns_to_exclude = ["password_digest"]
-    #(Member.column_names - columns_to_exclude).each do |c|
-    #  column c.to_sym
-   # end
-    #column :description do
-    # raw "<a class='view_description button'>View Description</a>"
-    #end
-  #end
+  index :download_links => false do
+    selectable_column
+    id_column
+    column :email
+    column :first_name
+    column :last_name
+    column :phone_number
+    column :graduation_year
+    column :major
+    column :member_status
+    column :points
+    actions
+  end
 
   batch_action :email, form: {subject: :text, message: :textarea}, confirm: "Please enter the subject and the message below" do |ids, inputs|
     batch_action_collection.find(ids).each do |member|
       UserMailer.contact_batch_email(member.first_name + " " + member.last_name, inputs[:message], inputs[:subject], member.email).deliver
     end
     redirect_to collection_path, notice: "The batch email has been sent to all the members you selected."
+  end
+
+  batch_action :approve do |ids|
+    batch_action_collection.find(ids).each do |member|
+      if member.member_status == "Pending"
+        member.update(member_status: "Approved")
+      end
+    end
+    redirect_to collection_path, notice: "The selected members have been approved."
+  end
+
+  batch_action :reject do |ids|
+    batch_action_collection.find(ids).each do |member|
+      if member.member_status == "Pending"
+        member.update(member_status: "Rejected")
+      end
+    end
+    redirect_to collection_path, notice: "The selected members have been rejected."
   end
 
   action_item :approve, priority: 0, only: :show do

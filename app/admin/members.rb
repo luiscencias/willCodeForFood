7104ea9870c85d34f951bb4ea1d13f61741acc78
jonathan.mejia <jonathan.mejia@tamu.edu>
2019-04-13 15:24:40 -0500

    can see all events associated with each member on admin

--- app/admin/members.rb
+++ app/admin/members.rb
@@ -4,4 +4,47 @@ ActiveAdmin.register Member do
   #action_item :approve_member, only: :show do
    # link_to "Approve",
   #end
+
+
+
+  # Below determines what the view link displays
+    show do
+      attributes_table do
+        row :first_name
+        row :last_name
+
+        row :email
+        row :phone_number
+
+        row :graduation_year
+        row :major
+
+        row :points
+
+        row :created_at
+        row :updated_at
+      end
+
+      active_admin_comments
+
+      # possible values for member status
+      # accepted, rejected, pending
+      panel "Registered Events" do
+        table_for member.events do
+          column :title
+
+          column :date
+          column :start_time
+          column :end_time
+
+          column :details
+          column :location
+
+          column :points
+        end
+      end
+
+
+    end
+
 end
